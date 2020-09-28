b8055737b6ccf0250866c1ad23458f0f54874501
lvsouza <ls18017@gmail.com>
2020-09-28 20:08:44 -0300

    feat: added libs, ts and configured .env

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+
+<h1 style="font-size:50px" align="center">Cadastro de alunos</h1>
+
+## License
+MIT @ [Lucas Souza](https://github.com/lvsouza)
---
+++ backend/knexfile.ts
@@ -0,0 +1,18 @@
+require('dotenv/config');
+import path from 'path';
+
+module.exports = {
+    client: 'pg',
+    connection: {
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS
+    },
+    migrations: {
+        directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+    }
+}
